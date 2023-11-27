ee9b6816af2306d85ad02cdbf1044b055c6b7586
Automatica <95876506+sauce36@users.noreply.github.com>
2023-11-27 19:26:06 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 let tg = window.Telegram.WebApp;
+
 let p = document.getElementById('p');
+let api = document.getElementById('api');
 let pop = document.getElementById('pop');
 let data = document.getElementById('data');
 let switch_button = document.getElementById('siq');
+
 
 switch_button.addEventListener('click', () => {
     p.innerHTML = '1';
@@ -12,4 +15,19 @@ pop.addEventListener('click', () => {
     p.innerHTML = tg.version;
     init = tg.initDataUnsafe;
     data.innerHTML = init.query_id;
+});
+api.addEventListener('click', async () => {
+    try {
+        let call = await fetch('https://emerging-quality-raccoon.ngrok-free.app/initData', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        let result = await call.json();
+        data.innerHTML = result;
+    } catch(error) {
+        data.innerHTML = error;
+    }
 });
