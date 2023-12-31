f8ee5801be3e761a3f0f31f7c21026d297de68a3
Automatica <95876506+sauce36@users.noreply.github.com>
2023-12-31 16:27:28 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ const cards = document.getElementById('cards');
 const cardsDesc = document.getElementById('cardsDesc');
 const body = document.getElementsByTagName('body')[0];
 
+const icons = document.getElementById('svg');
+
+
+window.addEventListener('popstate', async () => {
+    if (history.state == null) {
+        let cardDesc = cardsDesc.querySelector('[class="cardDesc"]');
+        await hideCard(cardDesc);
+    }
+});
 
 cardsDesc.addEventListener('transitionend', () => {
     if (cardsDesc.classList.contains('hide')) {
@@ -11,6 +20,9 @@ cardsDesc.addEventListener('transitionend', () => {
                 toggleClass(cDesc, 'hide')
             }
         }
+    };
+    if (history.state != null & cardsDesc.classList.contains('hide')) {
+        history.back();
     }
 });
 
@@ -22,7 +34,7 @@ cardsDesc.addEventListener('transitioncancel', () => {
         for (let i=c.length-1; i>0; i--) {
             toggleClass(c[i], 'hide')
         }
-    }
+    };
 });
 
 
@@ -32,7 +44,7 @@ function toggleClass(elem, cName) {
 
 
 async function getGoods() {
-    let resp = await fetch('https://emerging-quality-raccoon.ngrok-free.app/getGoods', {
+    let resp = await fetch('http://127.0.0.1:8000/getGoods', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -43,17 +55,19 @@ async function getGoods() {
 };
 
 async function createCard(item) {
-    let card = document.createElement('div');
+    let card = document.createElement('article');
     let cardDesc = document.createElement('div');
-    card.className = "card";
+    card.className = "item";
     cardDesc.classList.add('cardDesc', 'hide');
-    card.setAttribute('data-id', `${item.id}`);
     cardDesc.setAttribute('data-id', `${item.id}`);
     card.innerHTML = `
-        <img src="${item.photos[0]}" alt="${item.title} img">
-        <h3>${item.title}</h3>
-        <p>${item.desc}</p>
-        <p>${item.price} ${item.currency}</p>
+        <div class="card" data-id="${item.id}">
+            <img src="${item.photos[0]}" alt="${item.title} img">
+            <h3>${item.title}</h3>
+            <p class="desc">${item.desc}</p>
+            <p class="price">${item.price} ${item.currency}</p>
+        </div>
+        <button class="like" data-id="${item.id}">${icons.getElementsByClassName('like')[0].outerHTML}</button>
     `;
     cardDesc.innerHTML = `
         <button>Close</button>
@@ -81,17 +95,33 @@ let content = getGoods().then(async (result) => {
     for await (let item of result) {
         await createCard(item)
     };
-    let c = document.getElementsByClassName('card');
-    let cDesc = document.getElementsByClassName('cardDesc');
-    for await (let card of c) {
+    for await (let card of cards.getElementsByClassName('card')) {
         card.addEventListener('click', async (e) => {
+            if (history.state == null) {
+                history.pushState({'state':card.getAttribute('data-id')}, null, `${card.getAttribute('data-id')}`);
+            } else if (history.state.state != card.getAttribute('data-id')) {
+                history.replaceState({'state':card.getAttribute('data-id')}, null, `${card.getAttribute('data-id')}`);
+            };
             await hideCard(card);
             e.preventDefault();
         })
     };
-    for await (let card of cDesc) {
+    for await (let like of cards.querySelectorAll('button.like')) {
+        like.addEventListener('click', async (e) => {
+            like.classList.toggle('active');
+            if (like.classList.contains('active')) {
+                console.log(like.getAttribute('data-id'));
+            } else {
+                console.log('removed');
+            };
+            e.preventDefault();
+        })
+    };
+    for await (let card of cardsDesc.getElementsByClassName('cardDesc')) {
         card.getElementsByTagName('button')[0].addEventListener('click', async (e) => {
-            await hideCard(card);
+            if (history.state) {
+                history.back();
+            };
             e.preventDefault();
         })
     };
